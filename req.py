b5b568293c5cda66841a6d4c1a3e87a4baa0c2a3
younes4idhamou <95443121+younes4idhamou@users.noreply.github.com>
2022-02-08 18:21:11 +0100

    Update req.py

--- req.py
+++ req.py
@@ -5,15 +5,17 @@ import pandas as pd
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup,Comment
 import json
-import pathlib
+
+
 class pharmacie:
 
-    def __init__(self,nom="",ville="",adresse="",num="",cord=''):
+    def __init__(self,nom="",quartier="",adresse="",num="",cord='',lien=''):
         self.nom=nom
         self.adresse=adresse
         self.num=num
         self.cordonnee=cord
-        self.ville=ville
+        self.quartier=quartier
+        self.lien=lien
     def getNom(self):
         return self.nom
     def getAdresse(self):
@@ -22,10 +24,12 @@ class pharmacie:
         return self.num
     def getCordonnee(self):
         return self.cordonnee
-    def getVille(self):
-        return self.ville
+    def getlien(self):
+        return self.lien
+    def getQuartier(self):
+        return self.quartier
     def __str__(self):
-        return self.ville+": ["+self.nom+", "+self.adresse+", "+self.num+", "+self.cordonnee+"],"
+        return self.quartier+": ["+self.nom+", "+self.adresse+", "+self.num+", "+self.cordonnee+"],"
     
 pharmacies=[]
 for ville in open('villes.txt','r'):
@@ -36,19 +40,22 @@ for ville in open('villes.txt','r'):
     article=soup.find_all("article")
     for a in article:
         page=a.find_all('div',{'class':'btn-results-right'})
+        m=''
         if len(page)!=0:
             link=page[0].a.get('href')
-            req=Request('https://www.telecontact.ma'+link, headers={'User-Agent': 'Mozilla/5.0'})
+            m='https://www.telecontact.ma'+link
+            req=Request(m, headers={'User-Agent': 'Mozilla/5.0'})
             webpage = urlopen(req).read()
             soup=BeautifulSoup(webpage,'html.parser')
             comment = soup.find(text=lambda text:isinstance(text, Comment))
         else:
             comment=''
         
-        pharmacies.append([ville.replace('\n',''),a.a.text,a.find_all('span',{'itemprop':'streetAddress'})[0].span.text[2:],a.find_all('div',{'class':'tel'})[0].text[2:],comment])
-df2 = pd.DataFrame(pharmacies,columns=['ville', 'nom', 'adresse','tel','cordonnee'])
-out='['+df2.to_json(orient='records')[1:-1].replace('},{', '},{')+']'
-with open('dat.json', 'w') as f:
+        pharmacies.append([a.a.text,m,ville.replace('\n',''),a.find_all('span',{'itemprop':'streetAddress'})[0].span.text[2:-3],comment.replace('[','').replace(']',''),a.find_all('div',{'class':'tel'})[0].text[2:]])
+df2 = pd.DataFrame(pharmacies,columns=['pharmacie', 'lien', 'quartier','adresse','coordonnee','telephone'])
+out="["+df2.to_json(orient='records')[1:-1].replace('},{', '},{')+"]"
+print(out)
+output=open('dat.json', 'w')
+with output as f:
     f.write(out)
-
 
